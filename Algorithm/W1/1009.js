eb925def183c540d4dccd3dc27db17d5a3a62410
mina-gwak <mina.gwak@kakao.com>
2022-01-05 15:42:27 +0900

    feat: Solve 1009 problem

---
+++ Algorithm/W1/1009.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+let [n, ...arr] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+for (let i = 0; i < arr.length; i++) {
+  let [base, exponent] = arr[i].split(' ');
+  let result = 1;
+
+  for (let j = 1; j <= exponent; j++) {
+    result = result * base % 10;
+  }
+  console.log(result === 0 ? 10 : result);
+}
