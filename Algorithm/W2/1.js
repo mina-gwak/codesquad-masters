c09eab094161eb4476eb0d4618d07a3178e7673e
mina-gwak <mina.gwak@kakao.com>
2022-01-12 12:02:16 +0900

    feat: Solve 1 problem

---
+++ Algorithm/W2/1.js
@@ -0,0 +1,15 @@
+// 1. Two Sum
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (parseInt(nums[i]) + parseInt(nums[j]) === target) return [i, j];
+    }
+  }
+};
