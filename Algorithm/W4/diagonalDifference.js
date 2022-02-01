aa145afec06449de9b5bbd6e832a83a927538928
mina-gwak <mina.gwak@kakao.com>
2022-02-01 10:07:50 +0900

    fix: Fix diagonal difference solution

--- Algorithm/W4/diagonalDifference.js
+++ Algorithm/W4/diagonalDifference.js
@@ -2,11 +2,8 @@
 // https://www.hackerrank.com/challenges/diagonal-difference/problem
 
 function diagonalDifference(arr) {
-  const len = arr.length;
   const sums = arr.reduce((prev, cur, idx) => {
-    prev[0] += cur[idx];
-    prev[1] += cur[len - idx - 1];
-    return prev;
+    return [prev[0] + cur[idx], prev[1] + cur[arr.length - idx - 1]];
   }, [0, 0]);
-  return sums[0] > sums[1] ? Math.abs(sums[0] - sums[1]) : Math.abs(sums[1] - sums[0]);
+  return Math.abs(sums[0] - sums[1]);
 }
